ea9c2aeb3311da52be835f0575f81d9faa068e8a
Steve Cone <stephen.cone@nhs.netm>
2021-12-19 19:38:59 +0000

    completed turtle game

--- main.py
+++ main.py
@@ -10,25 +10,33 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.move, "Up")
 
+scrolling_speed = 1 / (scoreboard.score / 0.1)
+
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(scrolling_speed)
     screen.update()
     car_manager.new_car()
     car_manager.car_move()
+    scoreboard.show_score()
 
     # detect collision with a car
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # detect turtle making it to the other side!
     if player.ycor() == 300:
         player.reset()
+        scoreboard.level_up()
+
+
 
 
 
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,27 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.score = 1
+
+    def show_score(self):
+        self.goto(-250, 260)
+        self.write(f"Level: {self.score}", align="left", font=FONT)
+
+    def level_up(self):
+        self.clear()
+        self.score += 1
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
+
+
